68db75636e7960ccdf42a14af81d4ff51f848b62
Nelzee <nelzee07@gmail.com>
2024-08-17 13:10:56 +0200

    added api call for each word

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Word.css";
 
 const Word = ({ word }) => {
-  return <div>{word}</div>;
+  const [selectedWord, setSelectedWord] = useState(false);
+  useEffect(() => {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.trim()}`, {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(word, data));
+  }, [word]);
+
+  return (
+    <span
+      className={`word ${selectedWord ? "selected" : ""}`}
+      onClick={() => setSelectedWord(!selectedWord)}
+    >{`${word}`}</span>
+  );
 };
 
 export default Word;
